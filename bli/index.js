eef108aece9ac4cb3277a51ed070ca142cc2ef03
BioNetworkDesign <BioNetworkDesign@outlook.com>
2021-11-01 20:25:42 +0800

    Site updated: 2021-11-01 20:25:42

--- bli/index.js
+++ bli/index.js
@@ -5,7 +5,7 @@ new Vue({
   data: function () {
     return {
       content: [],
-      listData: [1, 2, 3, 4, 5, 6],
+      listData: [1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14],
       classOption: {
         limitMoveNum: 2,
         direction: 3,
